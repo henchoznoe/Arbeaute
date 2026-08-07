d64580d9a6bbb4ff4975e62efdebe9be5090a4c6
Noé Henchoz <henchoznoe@gmail.com>
2026-08-07 20:46:50 +0200

    feat: change logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,9 +71,21 @@ export const metadata: Metadata = {
     icon: [
       { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      {
+        url: '/favicon/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
     ],
     apple: '/favicon/apple-touch-icon.png',
     shortcut: '/favicon/favicon.ico',
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/favicon/safari-pinned-tab.svg',
+        color: siteConfig.themeColor,
+      },
+    ],
   },
 }
 
--- app/manifest.ts
+++ app/manifest.ts
@@ -21,6 +21,36 @@ const manifest = (): MetadataRoute.Manifest => ({
       sizes: '180x180',
       type: 'image/png',
     },
+    {
+      src: '/favicon/android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'any',
+    },
+    {
+      src: '/favicon/android-chrome-256x256.png',
+      sizes: '256x256',
+      type: 'image/png',
+      purpose: 'any',
+    },
+    {
+      src: '/favicon/icon-512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any',
+    },
+    {
+      src: '/favicon/maskable-icon-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
+    {
+      src: '/favicon/maskable-icon-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
   ],
 })
 
